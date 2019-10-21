afeafee00b77431b125bb2a488714881b06df334
tgattaz <thomas.gattaz@gmail.com>
2019-10-21 15:42:57 +0200

    how to http get

--- routes/arxiv.ts
+++ routes/arxiv.ts
@@ -1,13 +1,17 @@
-/* Routes de arxiv  */
-
-
+var request = require('request');
 var router = require('express').Router();
 
 /* Route pour récupérer la todolist  */
-router.get('/', function(req, res) {
+router.get('/', function(req, res1) {
+    
+    request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log(body.url);
+        console.log(body.explanation);
+    });
 
-    res.send("romain est un bon chum qui rentre manger chez lui parce que le RU c'est dégueulasse ....");
-
+    res1.send(res1);
 });
+
 
 module.exports = router;
